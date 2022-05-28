614f8b8b1ffdf6d975bb4d3cbb805f2197a08feb
andyping0522 <80187273+andyping0522@users.noreply.github.com>
2022-05-28 12:59:43 +1000

    Multiuser draw

--- src/board/BoardApp.java
+++ src/board/BoardApp.java
@@ -16,23 +16,20 @@ import java.util.ArrayList;
 
 import remote.IRemoteBoard;
 
-public class BoardApp extends JFrame implements ActionListener, MouseListener {
+public class BoardApp extends JFrame implements ActionListener {
 
     private boolean isManager;
-    private String actionType;
-    private int x1;
-    private int x2;
-    private int y1;
-    private int y2;
-    private IRemoteBoard board;
-    private ArrayList<Shape> shapes;
+
+
+    private WhiteBoard whiteBoard;
+
 
     public BoardApp(boolean isManager, IRemoteBoard board) {
         this.isManager = isManager;
         this.setTitle("Shared White Board");
-        this.board = board;
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.actionType = "Rectangle";
+
         if (this.isManager) {
             // display file menu
         }
@@ -59,14 +56,15 @@ public class BoardApp extends JFrame implements ActionListener, MouseListener {
         buttonPanel.add(rectangleBtn);
         buttonPanel.add(textBtn);
         buttonPanel.add(triangleBtn);
-        JPanel canvas = new JPanel();
-        canvas.setLayout(new BorderLayout());
-        canvas.setBounds(10, 30, 600, 600);
-        canvas.setBackground(Color.WHITE);
+        this.whiteBoard = new WhiteBoard(board);
+        whiteBoard.setLayout(new BorderLayout());
+        whiteBoard.setBounds(5, 10, 600, 600);
+
         this.setLayout(new BorderLayout());
-        this.addMouseListener(this);
         this.add(buttonPanel, BorderLayout.NORTH);
-        this.add(canvas, BorderLayout.CENTER);
+        this.add(whiteBoard, BorderLayout.CENTER);
+
+
         this.pack();
 
     }
@@ -79,156 +77,8 @@ public class BoardApp extends JFrame implements ActionListener, MouseListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        actionType = e.getActionCommand();
+        whiteBoard.setType(e.getActionCommand());
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
 
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-        this.x1 = e.getX();
-        this.y1 = e.getY();
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        this.x2 = e.getX();
-        this.y2 = e.getY();
-
-        switch (this.actionType) {
-            case "Line":
-                try {
-                    this.board.drawLine(x1, y1, x2, y2);
-                } catch (RemoteException remoteException) {
-                    remoteException.printStackTrace();
-                }
-                break;
-
-
-            case "Circle":
-                try {
-                    int width = Math.abs(x2 - x1);
-                    int height = Math.abs(y2 - y1);
-                    this.board.drawCircle((int)this.getCoord().getX(), (int)this.getCoord().getY(), width, height);
-
-                } catch (RemoteException remoteException) {
-                    remoteException.printStackTrace();
-                }
-                break;
-
-            case "Rectangle":
-
-                try {
-                    int width = Math.abs(x2 - x1);
-                    int height = Math.abs(y2 - y1);
-                    this.board.drawRectangle((int)this.getCoord().getX(), (int)this.getCoord().getY(), width, height);
-
-                } catch (RemoteException remoteException) {
-                    remoteException.printStackTrace();
-                }
-                break;
-
-            case "Triangle":
-                try {
-                    int width = Math.abs(x2 - x1);
-                    int height = Math.abs(y2 - y1);
-                    this.board.drawTriangle(x1, y1, x2, y2);
-                } catch (RemoteException remoteException) {
-                    remoteException.printStackTrace();
-
-                }
-                break;
-
-            case "Text":
-                try {
-                    String text = showInputDialog("Enter text: ");
-                    this.board.drawText(text, (int)this.getCoord().getX(), (int)this.getCoord().getY());
-                    //this.repaint();
-                } catch (RemoteException remoteException) {
-                    remoteException.printStackTrace();
-                }
-
-        }
-        //this.repaint();
-
-        
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    private Point getCoord() {
-        if (x1 < x2 && y1 < y2) {
-            return new Point(x1, y2);
-        } else if (x1 < x2 && y1 > y2) {
-            return new Point(x1, y1);
-        } else if (x1 > x2 && y1 > y2) {
-            return new Point(x2, y1);
-        } else {
-            return new Point(x2, y2);
-        }
-    }
-
-    @Override
-    public void paint(Graphics g){
-        paintComponents(g);
-        g.drawLine(199, 100, 250, 300);
-        try {
-//            byte[] img = this.board.getBoard();
-//            ByteArrayInputStream in = new ByteArrayInputStream(img);
-//            BufferedImage g2 = ImageIO.read(in);
-            //System.out.println(img[0]);
-            //System.out.println(in.toString());
-            //g.drawImage(g2, 10, 30, 600, 600, null);
-
-            ArrayList<Shape> shapes = this.board.getComponents();
-            //System.out.println(shapes.toString());
-            for (Shape shape:shapes) {
-                Graphics2D g3 = (Graphics2D)g;
-                g3.draw(shape);
-            }
-            this.repaint();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-//    @Override
-//    public void paintComponents(Graphics g) {
-//        super.paintComponents(g);
-//        //g.drawLine(199, 100, 250, 300);
-//        try {
-//
-////            InputStream in = new ByteArrayInputStream(img);
-////            BufferedImage g2 = ImageIO.read(in);
-//
-////            g.drawImage(g2, 10, 30, 10, 10, null);
-//            //g.drawLine(2, 2, 3, 3);
-//            //g.drawLine(199, 100, 250, 300);
-//            ArrayList<Shape> shapes = this.board.getComponents();
-//            for (Shape shape:shapes) {
-//                Graphics2D g3 = (Graphics2D)g;
-//                g3.draw(shape);
-//            }
-//            this.repaint();
-//        } catch (RemoteException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
--- src/board/WhiteBoard.java
+++ src/board/WhiteBoard.java
@@ -1,17 +1,38 @@
 package board;
 
+import remote.IRemoteBoard;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.EventListener;
 
-public class WhiteBoard extends JPanel implements MouseListener, EventListener {
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class WhiteBoard extends JPanel implements MouseListener{
 
     private String type;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private IRemoteBoard board;
 
-    public WhiteBoard() {
+
+    public WhiteBoard(IRemoteBoard board) {
         type = "Rectangle";
+        this.board = board;
+        setBackground(Color.WHITE);
+        addMouseListener(this);
 
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -21,10 +42,72 @@ public class WhiteBoard extends JPanel implements MouseListener, EventListener {
     @Override
     public void mousePressed(MouseEvent e) {
 
+        this.x1 = e.getX();
+        this.y1 = e.getY();
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        this.x2 = e.getX();
+        this.y2 = e.getY();
+
+        switch (this.type) {
+            case "Line":
+                try {
+                    this.board.drawLine(x1, y1, x2, y2);
+                } catch (RemoteException remoteException) {
+                    remoteException.printStackTrace();
+                }
+                break;
+
+
+            case "Circle":
+                try {
+                    int width = Math.abs(x2 - x1);
+                    int height = Math.abs(y2 - y1);
+                    this.board.drawCircle((int)this.getCoord().getX(), (int)this.getCoord().getY(), width, height);
+
+                } catch (RemoteException remoteException) {
+                    remoteException.printStackTrace();
+                }
+                break;
+
+            case "Rectangle":
+
+                try {
+                    int width = Math.abs(x2 - x1);
+                    int height = Math.abs(y2 - y1);
+                    this.board.drawRectangle((int)this.getCoord().getX(), (int)this.getCoord().getY(), width, height);
+
+                } catch (RemoteException remoteException) {
+                    remoteException.printStackTrace();
+                }
+                break;
+
+            case "Triangle":
+                try {
+                    int width = Math.abs(x2 - x1);
+                    int height = Math.abs(y2 - y1);
+                    this.board.drawTriangle(x1, y1, x2, y2);
+                } catch (RemoteException remoteException) {
+                    remoteException.printStackTrace();
+
+                }
+                break;
+
+            case "Text":
+                try {
+                    String text = showInputDialog("Enter text: ");
+                    this.board.drawText(text, (int)this.getCoord().getX(), (int)this.getCoord().getY());
+                    //this.repaint();
+                } catch (RemoteException remoteException) {
+                    remoteException.printStackTrace();
+                }
+
+        }
+        //this.repaint();
+
 
     }
 
@@ -36,5 +119,47 @@ public class WhiteBoard extends JPanel implements MouseListener, EventListener {
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+
+    private Point getCoord() {
+        if (x1 < x2 && y1 < y2) {
+            return new Point(x1, y2);
+        } else if (x1 < x2 && y1 > y2) {
+            return new Point(x1, y1);
+        } else if (x1 > x2 && y1 > y2) {
+            return new Point(x2, y1);
+        } else {
+            return new Point(x2, y2);
+        }
+    }
+
+
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //setIgnoreRepaint(true);
+        g.drawLine(199, 100, 250, 300);
+        try {
+
+
+            ArrayList<Shape> shapes = this.board.getComponents();
+            for (Shape shape:shapes) {
+                Graphics2D g3 = (Graphics2D)g;
+                g3.draw(shape);
+            }
+            this.repaint();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(600, 600);
     }
 }
